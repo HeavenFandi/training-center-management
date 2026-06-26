c915c8a742a3c6fcbb3cc94855c231da91869d17
heaven fandi <heavenfandi86@gmail.com>
2026-06-27 01:48:35 +0300

    Improve training session logic

--- src/pages/AdmainDashboard/CourseManagement.tsx
+++ src/pages/AdmainDashboard/CourseManagement.tsx
@@ -30,6 +30,7 @@ const CourseManagement = () => {
     adminCoursesLoading,
     tenantId,
     searchQuery,
+     creatingSession,
     handleSearch,
     handleClearSearch,
     handleDeleteCourse,
